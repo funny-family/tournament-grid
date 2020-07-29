e4c20ea11b2d2ef44c4fe322f2e5846ffc3d3884
funny family <maksvadik228@gmail.com>
2020-07-29 15:44:13 +0700

    added Number input component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="home">
+    <div class="table">
+      <div class="box aft"></div>
+      <div class="box"></div>
+    </div>
+    <hr>
+    <div>
+      <div class="tournament-grid">
+        фвфв
+      </div>
+    </div>
+    <hr>
     <Grid-setter
       placeholder="Enter an even number"
       type="text"
-      v-model="amountOfPlayers"
+      v-model="inputValue"
     />
+    <div v-if="!isNaN(amountOfPlayers)">{{ amountOfPlayers }}</div>
   </div>
 </template>
 
@@ -13,10 +25,54 @@ import GridSetter from '@/components/inputs/GridSetter.vue';
 
 export default {
   data: () => ({
-    amountOfPlayers: ''
+    inputValue: ''
   }),
   components: {
     'Grid-setter': GridSetter
+  },
+  computed: {
+    amountOfPlayers() {
+      return Number.parseInt(this.$data.inputValue, 10);
+    }
   }
 };
 </script>
+
+<style scoped>
+.tournament-grid {
+  background-color: #ebebeb;
+}
+
+/*           test           */
+.table {
+  position: relative;
+  width: 200px;
+  padding-right: 9px;
+  height: 100%;
+}
+
+.box {
+  width: 100%;
+  height: 40px;
+  background-color: aquamarine;
+  margin: 10px 0;
+}
+
+.box:not(:last-child) { /* set botton marign for all box classes but not for last! */
+  /* margin-bottom: 100px; */
+}
+
+.aft::after { /* link line between first and second player */
+  display: block;
+  width: 7px;
+  background-size: contain;
+  content: '';
+  position: absolute;
+  right: 0;
+  top: 20px;
+  bottom: 20px;
+  border: 2px solid #005050;
+  border-left: none;
+}
+/*           test           */
+</style>
