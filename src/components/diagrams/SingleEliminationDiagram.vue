c107e5dc99f26a85b66fedfef327c7f6eed4802b
funny family <maksvadik228@gmail.com>
2020-07-29 23:38:53 +0700

    added link lines to blocks

--- src/components/diagrams/SingleEliminationDiagram.vue
+++ src/components/diagrams/SingleEliminationDiagram.vue
@@ -32,6 +32,12 @@ export default {
 </script>
 
 <style scoped>
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+}
+
 .single-elimination-diagram-container {
   display: flex;
 }
@@ -42,6 +48,18 @@ export default {
   flex-direction: column;
 }
 
+.column:first-child .participant::before {
+  display: none;
+}
+
+.column:first-child .participant__content::before {
+  display: none;
+}
+
+.column:last-child .participant::after {
+  display: none;
+}
+
 .participant {
   display: flex;
   flex-direction: column;
@@ -50,6 +68,54 @@ export default {
   padding: 6px 0;
   flex-grow: 1;
   position: relative;
+}
+
+.participant::before {
+  content: '';
+  min-height: 30px;
+  border-left: 2px solid red;
+  position: absolute;
+  left: -10px;
+  top: 50%;
+  margin-top: -15px;
+  margin-left: -2px;
+}
+
+.participant:nth-child(odd)::after {
+  content: '';
+  border: 2px solid transparent;
+  border-top-color: red;
+  border-right-color: red;
+  height: 50%;
+  position: absolute;
+  right: -10px;
+  width: 12px;
+  top: 50%;
+  z-index: -1;
+}
+
+.participant:nth-child(even)::after {
+  content: '';
+  border: 2px solid transparent;
+  border-bottom-color: red;
+  border-right-color: red;
+  border-radius: 0%;
+  height: 50%;
+  position: absolute;
+  right: -10px;
+  width: 12px;
+  bottom: 50%;
+  z-index: -1;
+}
+
+.participant__content::before {
+  content: '';
+  width: 10px;
+  border-bottom: 2px solid red;
+  margin-left: -2px;
+  position: absolute;
+  top: 50%;
+  left: -10px;
 }
 
 .participant__content {
